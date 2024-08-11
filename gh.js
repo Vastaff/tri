a88769a586f64fc5b2608df32e360a6e0db38960
Vastaff <va2578735@gmail.com>
2024-08-11 06:31:54 +0300

    Update gh.js

--- gh.js
+++ gh.js
@@ -5,7 +5,7 @@ let sendTel = async () => {
   //console.log('Привет на Хабе и ${token}');
   //console.log(token);
   const messageText = 'Пивет с ГИТХАБа';
-  const bot = new Bot(TG_KEY);  
+  const bot = new Bot(token);  
   const message = bot.api.sendMessage(-1002091143933, messageText, {parse_mode: "HTML"});
   
 
